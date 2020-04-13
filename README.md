b3ce4d0a52283d30baf2fee1b0ce437951c39054
Federico <peconi.federico@gmail.com>
2020-04-13 14:46:10 +0000

    ignore SUSY and final notebook

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
 # kaf-nets
-
-## TODO
-
-* Write a short presentation (8 mins) using Beamer
-* Comment the whole project following the NumPy guidelines
-  And Professor's tips
-
-* Review the theory
--- kafnet.py
+++ kafnet.py
@@ -1,21 +1,46 @@
+
+# -*- coding: utf-8 -*-
+import numpy as np
 import tensorflow as tf
-import numpy as np 
 
 
 def dictionaryGen(D):
-    
-    """ Generate and return 
-        - D integers on the x axis evenly distributed around 0
-        - The step size Δx """
+    """ 
+    Dictionary generator
 
+    Parameters
+    ----------
+    D: int 
+       dictionary size
+
+    Returns
+    -------
+    tuple    
+        np array of D integers evenly distributed around 0,
+        the step size \gamma 
+    """
     d_pos = np.linspace(-3, 3, num= D, retstep=True, dtype=np.float32)
     return (d_pos[1], d_pos[0])
 
 
 def kafActivation(x, a, d, k_bwidth):
-
-    """ For each element in x, compute the weighted sum of 1D Gaussian kernel as defined in arXiv:1707.04035 """
+    """
+    For each element in x, compute the weighted sum of the 1D-Gaussian kernel
     
+    Parameters
+    ----------
+    x: tensor tf.float32
+       each element of the tensor is an activation for the kaf
+    
+    a: tensor tf.float32
+       tensor of mixing coefficients
+       
+    d: tensor tf.int32
+       dictionary tensor
+    
+    k_bwidth: tf.float32
+            kernel bandwidth
+    """
     x = tf.math.square(x - d)
     x = a * tf.math.exp((-k_bwidth) * x)
 
@@ -23,10 +48,9 @@ def kafActivation(x, a, d, k_bwidth):
 
 
 def kernelMatrix(d, k_bwidth):
-    
-    """ d is supposed to be a one dimensional (n, ) tensor 
-        return the kernel matrix K \in R^D*D where K_ij = ker(d_i, d_j) """
-
+    """ 
+    Return the kernel matrix K \in R^D*D where K_ij = ker(d_i, d_j) 
+    """
     d = tf.expand_dims(d, -1)
     return tf.exp(- k_bwidth * tf.square(d - tf.transpose(d)))
 
@@ -35,28 +59,34 @@ def kernelMatrix(d, k_bwidth):
 from tensorflow.keras.layers import Layer
 
 class Kaf(Layer):
-
-    """ Kernel Activation Function implemented as a keras layer to allow parameters learning 
+    """ 
+    Kernel Activation Function implemented as a keras layer to allow parameters learning
+    Detailed informations about the activation function can be found in the referenced paper 
     
-        It takes as input 'D' which is supposed to be the size of the dictionary
-        and the label 'conv' to indicate if the activation comes from a convolutional layer.
-        In particular, there are two supported activations only:
-        - A batch of flattened units i.e. of shape = (b, x)
-        - A batch of 2DConvolutions i.e. of shape = (b, x, y, f) where f is supposed to be the channels size
-        If the shape does not match with any of the latter, an error is thrown 
+    Parameters
+    ----------
+    D: int 
+       dictionary size
 
-        Moreover, another parameter 'ridge' \in {tanh, elu} if not None specifies how the mixing coefficients
-        need to be initialized in order to approximate the resulting Kaf to either a tanh or elu activation function
-        
-        References
-        ----------
+    conv: bool
+        indicates if the activations are coming from a convolutive layer or not
+        in particular, there are two supported activations only:
+            - A batch of flattened units i.e. of shape = (b, x)
+            - A batch of 2DConvolutions i.e. of shape = (b, x, y, f) where f is supposed to be the channels size
+        if the shape does not match with any of the latter, an error is thrown 
+
+    ridge: string
+        \in {tanh, elu, None} 
+        specifies how the mixing coefficients need to be initialized in order to approximate the resulting 
+        to either a tanh or elu activation function. If None than mixing coefficients are initialized randomly
+        again, if ridge assumes other values, a ValueError is fired
+
+    References
+    ----------
     [1] Scardapane, S., Van Vaerenbergh, S., Totaro, S. and Uncini, A., 2019. 
         Kafnets: kernel-based non-parametric activation functions for neural networks. 
         Neural Networks, 110, pp. 19-32.
-    [2] Marra, G., Zanca, D., Betti, A. and Gori, M., 2018. 
-        Learning Neuron Non-Linearities with Kernel-Based Deep Neural Networks. 
-        arXiv preprint arXiv:1807.06302."""
-
+   """
     def __init__(self, D, conv=False, ridge=None, **kwargs):
 
         super(Kaf, self).__init__()
@@ -101,17 +131,15 @@ class Kaf(Layer):
             
             x = tf.reshape(np.linalg.solve(K + eps*tf.eye(self.D), t), shape=(1, 1, -1)) # solve ridge regression and get 'a' coeffs
             a_init = x * tf.ones(shape=(1, input_shape[-1], self.D)) # reshape x
-            init = tf.constant_initializer(a_init.numpy()) # create an initializer from 'x'
            
             self.a = tf.Variable(initial_value=a_init, trainable=True, name = 'mix_coeffs')
-                         #self.add_weight(shape=(1, input_shape[-1], self.D),
-                     #               initializer = init,
-                     #               trainable=True)
 
         else:
+            # regularizer_l2 = tf.keras.regularizers.l2(0.0001)
             self.a = self.add_weight(shape=(1, input_shape[-1], self.D),
                                  name = 'mix_coeffs',   
-                                 initializer= 'random_normal', 
+                                 initializer= 'random_normal',
+                                 #regularizer= regularizer_l2,
                                  trainable=True) 
         
         # Adjust dimensions in order to exploit broadcasting and compute the entire batch all at once
@@ -123,19 +151,6 @@ class Kaf(Layer):
             self.d = tf.Variable(tf.reshape(self.d, shape=(1, 1, 1, 1, self.D)), name='dictionary', trainable=False)
 
 
-    def get_config(self): # override get_config to allow Kaf to be stored
-    
-        config = super().get_config().copy()
-        config.update({
-            'dict_size' : self.D,
-            'conv_flag' : self.conv,
-            'ridge_flag' : self.ridge, 
-            'dictionary' : self.d,        
-            'kernel_bwidth' : self.k_bandw,        
-            'mix_coeffs' : self.a  })
-        return config
-
     def call(self, inputs):
-        
         inputs = tf.expand_dims(inputs, -1)
         return kafActivation(inputs, self.a, self.d, self.k_bandw) 
